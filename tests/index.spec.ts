032a33f68e2672b6aaea09c4adf9bb174ae72cd3
Elle <epope@uw.edu>
2019-12-30 17:58:53 +0000

    feat: Created a testing 'schema' for drag and drop ui testing
    NOTE: Last test is having issues.... jasmine having problems requiring a component. May need to create that component test before it will work. Need to test this

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,27 +1,73 @@
 import { FormBuilder } from './../src/index';
+import { defaultOptions } from './../src/defaultOpts';
 
 describe('#formBuilder', () =>{
+    const idName = 'test';
+    let testBuilder: CustomObj;
+
     beforeEach(function() {
-        const testDiv = `<div id="test"></div>`;
+        const testDiv = `<div id="${idName}"></div>`;
 
         document.body.insertAdjacentHTML('afterbegin', testDiv);
+        testBuilder = new FormBuilder(idName);
     });
 
     afterEach(() => {
-        document.body.removeChild((document.getElementById('test') as HTMLElement));
+        document.body.removeChild((document.getElementById(idName) as HTMLElement));
     });
 
-    it('should instantiate based on element id', () =>{
-        const testBuilder = new FormBuilder('test');
-        console.log(testBuilder['list'].innerHTML);
+    it('should instantiate based on passed element id', () =>{
+
+        const testEl = document.getElementById(idName);
+        const testString = testEl ? testEl.innerHTML : '';
 
         expect((testBuilder['list'].innerHTML).length).not.toEqual(0);
+        expect(testString).toContain(testBuilder['list'].outerHTML);
+        expect(testString).toContain(testBuilder['wrapper'].outerHTML);
     })
 
     it('should not instantiate based on bad element id', () =>{
-        const testBuilder = new FormBuilder('wrong');
-        console.log(testBuilder['list'].innerHTML);
+        const failBuilder = new FormBuilder('wrong');
 
-        expect((testBuilder['list'].innerHTML).length).toEqual(0);
+        expect((failBuilder['list'].innerHTML).length).toEqual(0);
+    })
+
+    it('should change position of list items when dragged and dropped', ()=>{
+        // All Nodes list of all <li> items
+        const options = testBuilder.list.childNodes;
+        const li1 = options[0];
+        const li2 = options[1];
+
+        // Assert that the first list item is read as the first in the list object
+        expect(li1).toEqual(options[0]);
+
+        const dt = new DataTransfer();
+        const dragStart = new DragEvent('dragstart', { dataTransfer: dt });
+        const drop = new DragEvent('drop');
+        li1.dispatchEvent(dragStart);
+        li2.dispatchEvent(drop);
+
+        // Assert that the same list item is no longer first in the list object
+        expect(li1).not.toEqual(options[0]);
+
+        // // This is no longer blank!  Set in class
+        // console.log(dt.getData('text/html'));
+    })
+
+    it('should create a new form object when list item dropped in wrapper', ()=>{
+        const options = testBuilder.list.childNodes;
+        const li1 = options[0];
+
+        console.log((testBuilder.wrapper));
+        // const oldLength = (testBuilder.wrapper.innerHTML).length;
+
+        const dt = new DataTransfer();
+        const dragStart = new DragEvent('dragstart', { dataTransfer: dt });
+        const drop = new DragEvent('drop');
+
+        li1.dispatchEvent(dragStart);
+        testBuilder.wrapper.dispatchEvent(drop);
+
+        console.log(testBuilder.wrapper);
     })
 })
